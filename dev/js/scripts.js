fe427e38916bc9fccb8d6880c3f4734bbd0b59c0
Josh <josh@gdovindesigns.com>
2021-04-20 20:14:08 -0400

    added background ani

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,19 @@
 import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+
+import {numberThing} from "./demo-this"
+
+import { zoomTL } from "./zoomIn"
+import { fadeInTL } from "./fadeIn"
+
+gsap.registerPlugin(GSDevTools);
+
+let mainTL = gsap.timeline();
+
+mainTL.add(fadeInTL)
+        .add(zoomTL);
 
 
-gsap.to("#moon",{scale:5, alpha:0, duration:5});
+// console.log(numberThing);
+
+GSDevTools.create();

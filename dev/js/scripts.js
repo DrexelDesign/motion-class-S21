6288f712c45afb6c3ea056263d33ef8afdfe8cd8
Josh <josh@gdovindesigns.com>
2021-04-27 20:40:21 -0400

    updated demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,16 +4,20 @@ import { GSDevTools } from "gsap/GSDevTools";
 import { zoomTL } from "./zoomIn"
 import { fadeInTL } from "./fadeIn"
 import { spaceShipTL } from "./space-ship"
+import { liftOffTL } from "./listOff"
+import { flightTL } from "./flightPath"
+
 
 gsap.registerPlugin(GSDevTools);
 
 let mainTL = gsap.timeline({paused:true});
 
 mainTL.add(fadeInTL)
-.addLabel("marker")
         .add(zoomTL)
-        
-        .add(spaceShipTL);
+        .add(spaceShipTL)
+        .add(liftOffTL)
+        .addLabel("marker")
+        .add(flightTL);
 
         mainTL.play("marker");
 

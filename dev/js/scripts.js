505e85d84bb62e3ee85ea18986351438d67d436d
Josh <josh@gdovindesigns.com>
2021-05-27 19:02:30 -0400

    updated demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,9 +6,11 @@ gsap.registerPlugin(DrawSVGPlugin);
 
 let drawTL = gsap.timeline();
 
-drawTL.to("#myPath",{duration:2, drawSVG:0});
-// drawTL.from("#myPath",{duration:2, drawSVG:0});
-// drawTL.to("#myPath",{duration:2, drawSVG:"50% 50%" });
+// gsap.set("#myPath", { scaleX: -1, scaleY:-1, transformOrigin: "center center" });
 
-// drawTL.fromTo("#myPath",{drawSVG:"0% 10%" },{duration:2, drawSVG:"95% 100%", ease:"bounce.out"})
-//     .to("#myPath",{duration:2, drawSVG:"0% 100%"});
+// drawTL.to("#myPath",{duration:2, drawSVG:0});
+// drawTL.from("#myPath",{duration:2, drawSVG:0});
+// drawTL.from("#myPath",{duration:2, drawSVG:"60% 200px" });
+
+drawTL.fromTo("#myPath",{drawSVG:"0% 10%" },{duration:2, drawSVG:"95% 100%", ease:"bounce.out"})
+    .to("#myPath",{duration:2, drawSVG:"0% 100%"});

62462d577c3d9d997d3273c2c61c59b7391110a3
Josh <josh@gdovindesigns.com>
2021-06-01 19:33:58 -0400

    class demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,11 +2,23 @@
 
 import $ from "jquery"
 
+let musicIsPlaying = false;
+
 let audio = document.getElementById("audio");
+// volume is 0 - 1
+// audio.volume = 0.8;
 
 $("#box").on("click",function(){
-    console.log("play");
-    audio.play();
+
+    if(musicIsPlaying === false){
+        // console.log("play");
+        audio.play();
+        musicIsPlaying = true;
+    }else{
+        audio.pause();
+        // audio.stop();
+        musicIsPlaying = false;
+    }
 })
 
 

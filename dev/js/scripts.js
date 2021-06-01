2d8d877f2c486c1552aa57ff24b5b5dff08e1397
Josh <josh@gdovindesigns.com>
2021-06-01 18:06:21 -0400

    repeating scroll

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,3 +5,9 @@ let textLength = $("#scrollText").width();
 console.log(textLength + " is the text width");
 
 gsap.to("#scrollText",{duration:20, x:-textLength, ease:"none"});
+
+
+// let textLength = $("#scrollContainer").width();
+// console.log(textLength + " is the text width");
+
+// gsap.to("#scrollContainer",{duration:3, x:"-=340", ease:"none", repeat:-1});

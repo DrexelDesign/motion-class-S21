7af4c8a2a34334e90b54a05bfcd5c64ddb234044
Josh <josh@gdovindesigns.com>
2021-06-01 19:04:59 -0400

    demo morph

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,6 +10,7 @@ let morphTL = gsap.timeline();
 
 // morphTL.to("#box", {duration: 1, morphSVG:"#star"});
 
-// morphTL.to("#box", {duration: 1, morphSVG:"#star",rotation:180, fill:"#000"});
+morphTL.to("#box", {duration: 1, morphSVG:"#star",rotation:180, fill:"#000",ease: "bounce.out"});
+        // .to("#box", {duration: 1, morphSVG:"#hello", fill:"#FB1515"});
 
 // morphTL.to("#box", {duration: 1, morphSVG:"#hello", fill:"#FB1515"});
